e8c1acb13457cc3b71a6eebb353f3b84c1c6f24a
Yashmika Anusara <yashmikaaunusara@gmail.com>
2022-10-12 14:00:36 +0530

    Update Main.jsx

--- project/src/Pages/Main.jsx
+++ project/src/Pages/Main.jsx
@@ -9,14 +9,140 @@ const rows1 = [
     domain: "Jon",
     tld: 35,
     status: "Active",
-    adlut: "Test",
     keywords: "Test Test",
+  },
+  {
+    id: 2,
+    lastName: "Snow1",
+    domain: "Jon1",
+    tld: 31,
+    status: "Active1",
+    keywords: "Test1 Test1",
+  },
+  {
+    id: 3,
+    lastName: "Snow2",
+    domain: "Jon2",
+    tld: 32,
+    status: "Active2",
+    keywords: "Test2 Test2",
+  },
+  {
+    id: 4,
+    lastName: "Snow3",
+    domain: "Jon4",
+    tld: 35,
+    status: "Active5",
+    keywords: "Test2 Test3",
+  },
+  {
+    id: 5,
+    lastName: "Sno1w",
+    domain: "Jon3",
+    tld: 3545,
+    status: "Active3",
+    keywords: "Test3 Test1",
+  },
+  {
+    id: 16,
+    lastName: "Snow 56",
+    domain: "Jon57",
+    tld: 36,
+    status: "Active75",
+    keywords: "Test57 Test57",
+  },
+  {
+    id: 57,
+    lastName: "Snow57",
+    domain: "Jon",
+    tld: 57,
+    status: "Active57",
+    keywords: "Test57 Test57",
+  },
+
+  {
+    id: 8,
+    lastName: "Snow8",
+    domain: "Jon8",
+    tld: 38,
+    status: "Active8",
+    keywords: "Test8 Test8",
+  },
+  {
+    id: 546,
+    lastName: "Snow536",
+    domain: "Jon45",
+    tld: 435,
+    status: "Active435",
+    keywords: "Tes435t Test435",
+  },
+  {
+    id: 67,
+    lastName: "Snow453",
+    domain: "Jon54",
+    tld: 455,
+    status: "Active43",
+    keywords: "Test345 Test345",
+  },
+  {
+    id: 88,
+    lastName: "Snow76",
+    domain: "Jon67",
+    tld: 765,
+    status: "Active67",
+    keywords: "Test67 Test67",
+  },
+  {
+    id: 988,
+    lastName: "Snow34",
+    domain: "Jon65",
+    tld: 345,
+    status: "Active45",
+    keywords: "Test54t Test54",
+  },
+  {
+    id: 45,
+    lastName: "Snow45",
+    domain: "Jon45",
+    tld: 3455,
+    status: "Active45",
+    keywords: "Test54 Test45",
   },
 ];
 
 const rows2 = [
   {
     id: 1,
+    tld: 35,
+    renewal: "Test Test",
+  },
+  {
+    id: 2,
+    tld: 35,
+    renewal: "Test Test",
+  },
+  {
+    id: 3,
+    tld: 35,
+    renewal: "Test Test",
+  },
+  {
+    id: 4,
+    tld: 35,
+    renewal: "Test Test",
+  },
+  {
+    id: 5,
+    tld: 35,
+    renewal: "Test Test",
+  },
+  {
+    id: 6,
+    tld: 35,
+    renewal: "Test Test",
+  },
+  {
+    id: 7,
     tld: 35,
     renewal: "Test Test",
   },
@@ -28,11 +154,66 @@ const rows3 = [
     name: "Test",
     email: "test@test.com",
   },
+  {
+    id: 2,
+    name: "Test",
+    email: "test@test.com",
+  },
+  {
+    id: 3,
+    name: "Test",
+    email: "test@test.com",
+  },
+  {
+    id: 4,
+    name: "Test",
+    email: "test@test.com",
+  },
+  {
+    id: 5,
+    name: "Test",
+    email: "test@test.com",
+  },
+  {
+    id: 6,
+    name: "Test",
+    email: "test@test.com",
+  },
+  {
+    id: 7,
+    name: "Test",
+    email: "test@test.com",
+  },
+  {
+    id: 8,
+    name: "Test",
+    email: "test@test.com",
+  },
 ];
 
 const rows4 = [
   {
     id: 1,
+    name: "Test",
+  },
+  {
+    id: 2,
+    name: "Test",
+  },
+  {
+    id: 3,
+    name: "Test",
+  },
+  {
+    id: 4,
+    name: "Test",
+  },
+  {
+    id: 5,
+    name: "Test",
+  },
+  {
+    id: 6,
     name: "Test",
   },
 ];
@@ -42,13 +223,61 @@ const rows5 = [
     id: 1,
     address: "Test,test7",
   },
+  {
+    id: 2,
+    address: "Test,test7",
+  },
+  {
+    id: 3,
+    address: "Test,test7",
+  },
+  {
+    id: 4,
+    address: "Test,test7",
+  },
+
+  {
+    id: 5,
+    address: "Test,test7",
+  },
+  {
+    id: 6,
+    address: "Test,test7",
+  },
 ];
 
 const rows6 = [
   {
     id: 1,
     keyword: "Test",
-    adult: "Child",
+  },
+  {
+    id: 2,
+    keyword: "Test",
+  },
+  {
+    id: 3,
+    keyword: "Test",
+  },
+  {
+    id: 4,
+    keyword: "Test",
+  },
+  {
+    id: 5,
+    keyword: "Test",
+  },
+  {
+    id: 6,
+    keyword: "Test",
+  },
+  {
+    id: 7,
+    keyword: "Test",
+  },
+  {
+    id: 8,
+    keyword: "Test",
   },
 ];
 
@@ -57,8 +286,14 @@ const columns1 = [
   { field: "domain", headerName: "Domain", width: 220, editable: true },
   { field: "tld", headerName: "TLD", width: 220, editable: true },
   { field: "status", headerName: "Status", width: 220, editable: true },
-  { field: "adlut", headerName: "Adlut", width: 220, editable: true },
-  { field: "keywords", headerName: "Keywords", width: 330,editable: true },
+  {
+    field: "adlut",
+    headerName: "Adlut",
+    width: 220,
+    type: "boolean",
+    editable: true,
+  },
+  { field: "keywords", headerName: "Keywords", width: 330, editable: true },
 ];
 
 const columns2 = [
@@ -86,7 +321,13 @@ const columns5 = [
 const columns6 = [
   { field: "id", headerName: "ID", width: 120, editable: true },
   { field: "keyword", headerName: "Keyword", width: 420, editable: true },
-  { field: "adult", headerName: "Adult", width: 120, editable: true },
+  {
+    field: "adult",
+    headerName: "Adult",
+    width: 120,
+    editable: true,
+    type: "boolean",
+  },
 ];
 export default function Main() {
   return (
@@ -98,7 +339,7 @@ export default function Main() {
             rows={rows1}
             columns={columns1}
             pageSize={5}
-            checkboxSelection
+            // checkboxSelection
             experimentalFeatures={{ newEditingApi: true }}
           />
         </div>
@@ -111,7 +352,7 @@ export default function Main() {
               rows={rows2}
               columns={columns2}
               pageSize={5}
-              checkboxSelection
+              // checkboxSelection
             />
           </div>
         </div>
@@ -123,7 +364,7 @@ export default function Main() {
               rows={rows3}
               columns={columns3}
               pageSize={5}
-              checkboxSelection
+              // checkboxSelection
             />
           </div>
         </div>
@@ -134,7 +375,7 @@ export default function Main() {
               rows={rows4}
               columns={columns4}
               pageSize={5}
-              checkboxSelection
+              // checkboxSelection
             />
           </div>
         </div>
@@ -148,7 +389,7 @@ export default function Main() {
               rows={rows5}
               columns={columns5}
               pageSize={5}
-              checkboxSelection
+              // checkboxSelection
             />
           </div>
         </div>
@@ -159,7 +400,7 @@ export default function Main() {
               rows={rows6}
               columns={columns6}
               pageSize={5}
-              checkboxSelection
+              // checkboxSelection
             />
           </div>
         </div>
